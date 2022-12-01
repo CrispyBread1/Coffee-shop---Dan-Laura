9f37f2f36447c9b0547c7ca90b9a07be5de61043
Daniel <dsalvatori2008@gmail.com>
2022-12-01 10:04:05 +0000

    random changes

--- src/customer.py
+++ src/customer.py
@@ -1,8 +1,9 @@
 class Customer:
-    def __init__(self, name, wallet):
+    def __init__(self, name, age, wallet):
         self.customer_name = name 
+        self.age = age
         self.wallet = wallet
 
 
-
-    
+    def give_money_to_coffeeshop(self, drink):
+        self.wallet -= drink.price
